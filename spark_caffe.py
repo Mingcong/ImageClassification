95e22e8e26f086ae95611a3f7881737a7c370855
Mingcong <songmingcong@gmail.com>
2015-03-12 15:17:07 -0400

    save out and parse out in RDD

--- spark_caffe.py
+++ spark_caffe.py
@@ -23,10 +23,6 @@ from pyspark import SparkContext,SparkConf
 def main(argv):
     conf = SparkConf()
     sc = SparkContext(appName="PythonCaffe",pyFiles=['/usr/local/lib/python2.7/site-packages/cv2.so'],environment={'KMP_AFFINITY':'scatter','OMP_NUM_THREADS':'16'});
-    #sc = SparkContext(appName="PythonCaffe",pyFiles=['/usr/local/lib/python2.7/site-packages/cv2.so','/home/ideal/caffe-memory/build/lib/libcaffe.so']);
-    conf.setExecutorEnv("KMP_AFFINITY", "scatter")
-    conf.setExecutorEnv("GOMP_CPU_AFFINITY", "0-15")
-    conf.setExecutorEnv("OMP_NUM_THREADS", "16")
 
     pycaffe_dir = os.path.dirname("/home/ideal/caffe-memory/python/ImageClassification")
 
@@ -38,7 +34,11 @@ def main(argv):
     )
     parser.add_argument(
         "batch_size",
-        help="Input image, directory, or npy."
+        help="the size of Batch Processing."
+    )
+    parser.add_argument(
+        "outName",
+        help="Output Name."
     )
     # Optional arguments.
     parser.add_argument(
@@ -110,6 +110,11 @@ def main(argv):
     if args.channel_swap:
         channel_swap = [int(s) for s in args.channel_swap.split(',')]
 
+    batch_size = int(args.batch_size)
+    class_name = []
+    file = open("synset_words.txt")
+    for line in file:
+	class_name.append(line)
     def myFunc(s):
     	# Make classifier.
     	classifier = caffe.Classifier(args.model_def, args.pretrained_model,
@@ -117,7 +122,11 @@ def main(argv):
             input_scale=args.input_scale, raw_scale=args.raw_scale,
             channel_swap=channel_swap)
 	return classifier.predict(s,False)	
-
+    def myParse(s):
+	out = []
+	for i in range(0, batch_size):
+ 		out.append((str(s[0]*batch_size+i),class_name[s[1][i].argmax()]))		
+	return out
     if args.gpu:
         print 'GPU mode'
 
@@ -127,32 +136,19 @@ def main(argv):
     L = sc.binaryFiles(args.input_file + '/*.' + args.ext)
     #L = sc.binaryFiles('hdfs://dirt06:9000/100/ILSVRC2012_test_0000009*')
     #L = sc.binaryFiles('hdfs://localhost:9000/*.JPEG').take(batch_size)
-    #L = sc.binaryFiles('/home/ideal/ILSVRC2012_test/100/ILSVRC2012_test_00000100.JPEG').take(batch_size)
-    batch_size = int(args.batch_size)
     img = L.map(lambda s: (int(string.atoi(s[0][-13:-5])/batch_size),cv2.imdecode(np.asarray(bytearray(s[1]),dtype=np.uint8),1)/255.0))
     imgs = img.groupByKey()
     imgs = imgs.map(lambda s: (s[0],list(s[1]))) 
     # Classify.
     predicts = imgs.map(lambda s: (s[0],myFunc(s[1])))
-    class_name = []
-    file = open("synset_words.txt")
-    for line in file:
-	class_name.append(line)
-    results = predicts.collect()
-    for s in results:
-    	for i in range(0,batch_size):
-       		print s[0]*batch_size + i, ' predicted class:', s[1][i].argmax()
-       		print s[0]*batch_size + i, ' predicted class:', class_name[s[1][i].argmax()]
-		
-    
+    parsePredicts = predicts.flatMap(lambda s: myParse(s))
+    parsePredicts.saveAsTextFile(args.outName)
+    #results = parsePredicts.collect()
+    #for s in results:
+    #	print s	
     print "Done in %.2f s." % (time.time() - start)
     
-
     sc.stop()
-
-    # Save
-    #np.save(args.output_file, predictions)
-
 
 if __name__ == '__main__':
     main(sys.argv)
